4ee53314c4526fe8b61b27fc9939cc261b388b18
Chris Wessels <geebeish@mweb.co.za>
2023-03-16 22:07:30 +0200

    track set

--- src/firebase.js
+++ src/firebase.js
@@ -6,6 +6,7 @@ import { getAuth } from "firebase/auth";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
 //Go To Google FIREBASE to create your own set
